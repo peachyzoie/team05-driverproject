087e4aa011baf515471a4c34ab8cf083f9d3c54d
Abraham <anaula@clemson.edu>
2023-02-22 18:51:19 -0500

    cdk-backend checked for deployment

--- cdk-backend/lib/cdk-backend-stack.ts
+++ cdk-backend/lib/cdk-backend-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
