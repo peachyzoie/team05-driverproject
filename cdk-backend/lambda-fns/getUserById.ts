c28a9234b61c780a55b26de4cd6bb087a54b25ad
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-03-08 21:59:00 -0500

    prepared for deployment

--- cdk-backend/lambda-fns/getUserById.ts
+++ cdk-backend/lambda-fns/getUserById.ts
@@ -2,7 +2,7 @@ import db from './db';
 async function getUserById(userId: number){
     try {
         //NOTE: use of backticks ` ` for MySQL query
-        const query = `SELECT * FROM Users WHERE id = :userId`;
+        const query = `SELECT * FROM User WHERE id = :userId`;
         const results = await db.query(query, {userId});
         return results.records[0];
     }catch (err) {
