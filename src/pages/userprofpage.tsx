b817ba7f24e5758046d0c1c51f80daa59da05fd4
mackenzieb5 <92061540+mackenzieb5@users.noreply.github.com>
2023-03-14 15:48:10 -0400

    Create userprofpage.tsx
    Added another page file

---
+++ src/pages/userprofpage.tsx
@@ -0,0 +1,14 @@
+/*import Link from "next/link";
+import React from "react";
+
+export default function ProfilePage() {
+    return (
+        //Link for Login to... directs to some path specified from href, so basically no where for now
+        <div>
+            <Link href={"../src"}>
+                <h1>Your Profile</h1>
+            </Link>
+        </div>
+
+    );
+}*/
