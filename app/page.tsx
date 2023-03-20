9d287b7d7c53a6a7edf3cf007c3dadb08f965362
Zoie Rast <zoie.m.rast@gmail.com>
2023-03-20 17:36:47 -0400

    Stuff for making front end

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../src/aws-exports';
 import React from 'react';
 import Link from "next/link";
-//import { BrowserRouter } from 'react-router-dom';
+import { useRouter } from 'next/router';
 Amplify.configure(awsExports);
 
 /*
@@ -24,7 +24,6 @@ export default function Home() {
 */
 
 //With logins
-
 function Home() {
     return (
         <div>
@@ -32,99 +31,12 @@ function Home() {
             <Link href={"../src"}>
                 <h1>Login to the Driver Incentive Program</h1>
             </Link>
+            <li><Link href = "/src/pages/profile">
+                Profile Page
+            </Link></li>
         </div>
 
     );
 }
 
 export default withAuthenticator(Home);
-
-/*
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
-*/
---
+++ src/pages/sponsor_dashboard_pages/drivers.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function driver_list(){
+    return(
+        <>
+            <h1>Driver List</h1>
+            <h2>
+                <Link href = "/">
+                    <a>Dashboard</a>
+                </Link>
+            </h2>
+        </>
+    )
+}
