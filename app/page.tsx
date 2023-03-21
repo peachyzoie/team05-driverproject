b86406d78e2d21f9c7ca0f3d0244ce8920eb937e
Zoie Rast <zoie.m.rast@gmail.com>
2023-03-20 20:41:47 -0400

    Changes to pages

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,24 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../src/aws-exports';
 import React from 'react';
 import Link from "next/link";
-import { useRouter } from 'next/router';
+//import { useRouter } from 'next/router';
 Amplify.configure(awsExports);
+
+//With logins
+function Home() {
+    return (
+        <div>
+
+            <Link href={"../src"}>
+                <h1>Login to the Driver Incentive Program</h1>
+            </Link>
+            <li><Link href = "/src/pages/sponsor_dashboard">
+                Dashboard
+            </Link></li>
+        </div>
+
+    );
+}
 
 /*
 export default function Home() {
@@ -22,21 +38,5 @@ export default function Home() {
     );
 }
 */
-
-//With logins
-function Home() {
-    return (
-        <div>
-
-            <Link href={"../src"}>
-                <h1>Login to the Driver Incentive Program</h1>
-            </Link>
-            <li><Link href = "/src/pages/profile">
-                Profile Page
-            </Link></li>
-        </div>
-
-    );
-}
 
 export default withAuthenticator(Home);
