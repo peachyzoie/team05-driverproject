42821d39a434deaa58b43b9431c6cbb9245f43a4
Zoie Rast <zoie.m.rast@gmail.com>
2023-04-09 14:27:04 -0400

    Added some changed to the catalog

--- app/catalog_home/page.tsx
+++ app/catalog_home/page.tsx
@@ -4,8 +4,7 @@ import fetchTop5 from "@/app/catalog_home/fetchTop5";
 
 async function Catalog() {
     return (
-        //<h2>This is the catalog home page</h2>
-    fetchTop5()
+        fetchTop5()
     )
 }
 export default Catalog
