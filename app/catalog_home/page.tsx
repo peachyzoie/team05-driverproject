b0fd4ca06950f90406305c3225991b4fc849df2c
Chelsea van Vulpen <cjvvulpen@gmail.com>
2023-04-09 22:25:23 -0400

    x

--- app/catalog_home/page.tsx
+++ app/catalog_home/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import {FiBook, FiHeadphones, FiMusic, FiCoffee} from "react-icons/fi"
+import {FiBook, FiMonitor, FiMusic, FiFilm} from "react-icons/fi"
 import { RxChatBubble, RxDashboard} from "react-icons/rx";
 
 function CatalogHome() {
@@ -9,11 +9,11 @@ function CatalogHome() {
             <h1 className={"text-3xl font-bold"}>Catalog Directory</h1>
             <div  className="grid gap-16 grid-cols-fluid">
                 <Link href = "/catalog_home/top5Songs"><FiMusic />Top 5 Songs</Link>
-                <Link href = "/catalog_home/top5AudioBooks"><FiHeadphones />Top 5 Audio Books</Link>
-                <Link href = "/catalog_home/top5Books"><FiBook />Top 5 Books</Link>
+                <Link href = "/catalog_home/top5AudioBooks"><FiBook />Top 5 Audio Books</Link>
+                <Link href = "/catalog_home/top5Shows"><FiMonitor />Top 5 Shows</Link>
                 <Link href = "/catalog_home/top5Podcasts"><RxChatBubble />Top 5 Podcasts</Link>
                 <Link href = "/catalog_home/top5Apps"><RxDashboard />Top 5 Apps</Link>
-                <Link href = "/catalog_home/top5Movies">Top 5 Movies</Link>
+                <Link href = "/catalog_home/top5Movies"><FiFilm />Top 5 Movies</Link>
             </div>
         </div>
     )
