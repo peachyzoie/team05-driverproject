c64232170e812aec68112396fb2c500957c8f1b9
Chelsea van Vulpen <cjvvulpen@gmail.com>
2023-04-09 21:28:58 -0400

    Update page.tsx

--- app/catalog_home/page.tsx
+++ app/catalog_home/page.tsx
@@ -2,14 +2,15 @@ import Link from "next/link";
 
 function CatalogHome() {
     return (
-        <div>
+            <div>
         <Link href = "/catalog_home/top5Songs">
             <h1>Top 5 Songs</h1>
         </Link>
+
         <Link href = "/catalog_home/top5AudioBooks">
             <h1>Top 5 Books</h1>
         </Link>
-        </div>
+            </div>
     )
 }
 export default CatalogHome
