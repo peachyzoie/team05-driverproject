07ff2c882a6c5586f948cd5f157eb8683c34d36d
Chelsea van Vulpen <cjvvulpen@gmail.com>
2023-04-09 21:41:35 -0400

    Update page.tsx

--- app/catalog_home/top5AudioBooks/page.tsx
+++ app/catalog_home/top5AudioBooks/page.tsx
@@ -4,8 +4,7 @@ import fetchTop5 from "@/app/catalog_home/top5AudioBooks/fetchTop5";
 
 async function Catalog() {
     return (
-        <h1>Hello There</h1>
-        //fetchTop5()
+        fetchTop5()
     )
 }
 export default Catalog
