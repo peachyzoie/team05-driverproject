965b0865a9c609c64cfdb439b01f7cd2ae72f0a8
Zoie Rast <zoie.m.rast@gmail.com>
2023-04-09 21:36:31 -0400

    Added more functionalities

--- app/catalog_home/page.tsx
+++ app/catalog_home/page.tsx
@@ -8,7 +8,6 @@ function CatalogHome() {
             <div  className="grid gap-16 grid-cols-fluid">
                 <Link href = "/catalog_home/top5Songs">Top 5 Songs</Link>
                 <Link href = "/catalog_home/top5AudioBooks">Top 5 Audio Books</Link>
-
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'black-olive': '#37392E',
+      'payne-gray': '#19647E',
+      'verdigris': '#28AFB0',
+      'timberwolf': '#DDCECD',
+      'isabelline': '#EEE5E6',
+      'white': '#FFFFFF'
+    },
     extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
       gridTemplateColumns: {
         fluid: "repeat(auto-fit,minmax(15rem,1fr))",
+      },
+      borderRadius: {
+        '4xl': '2rem',
       }
     },
   },
