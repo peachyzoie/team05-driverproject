36614ae6913c08c4dc986ea209e81659829d4bd1
Zoie Rast <zoie.m.rast@gmail.com>
2023-04-12 09:33:05 -0400

    amplify pull changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "team5amplifye3f908d6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdministratorGroupRole": "string",
+      "DriverGroupRole": "string",
+      "SponsorGroupRole": "string"
+    }
+  }
+}
