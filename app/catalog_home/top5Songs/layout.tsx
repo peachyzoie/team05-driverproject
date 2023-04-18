19d2d39fe613c8a9272fda6820c2c5f2a077b6ee
cjBroom <90470010+cjBroom@users.noreply.github.com>
2023-04-18 11:53:19 -0400

    Created styles.module.css for top5Songs and edited layout.tsx accordingly.
    Created styles.module.css for top5Songs and edited layout.tsx accordingly.

--- app/catalog_home/top5Songs/layout.tsx
+++ app/catalog_home/top5Songs/layout.tsx
@@ -1,11 +1,15 @@
+import styles from './styles.module.css'
+
 export default function top5SongsLayout({
     children, }: {
         children: React.ReactNode
     }) {
-    return (
+    
+        /* This css changes only the top5Songs dir, and can be changed to make each fetch call unique to the given media. */
+        return (
         <>
             <nav>Please enjoy browsing the top 50 Songs from the iTunes store!</nav>
-            <main>
+            <main className={styles.main}>
                 {children}
             </main>
         </>
