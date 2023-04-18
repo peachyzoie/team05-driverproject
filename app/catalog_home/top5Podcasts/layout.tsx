78cd4789002d936bd4f1505d41bcedc181b8faae
cjBroom <90470010+cjBroom@users.noreply.github.com>
2023-04-18 11:49:50 -0400

    Created styles.module.css for top5Podcasts and edited layout.tsx accordingly.
    Created styles.module.css for top5Podcasts and edited layout.tsx accordingly.

--- app/catalog_home/top5Podcasts/layout.tsx
+++ app/catalog_home/top5Podcasts/layout.tsx
@@ -1,11 +1,15 @@
+import styles from './styles.module.css'
+
 export default function top5PodcastsLayout({
     children, }: {
         children: React.ReactNode
     }) {
-    return (
+    
+        /* This css changes only the top5Podcasts dir, and can be changed to make each fetch call unique to the given media. */
+        return (
         <>
             <nav>Please enjoy browsing the top 50 Podcasts from the iTunes store!</nav>
-            <main>
+            <main className={styles.main}>
                 {children}
             </main>
         </>
