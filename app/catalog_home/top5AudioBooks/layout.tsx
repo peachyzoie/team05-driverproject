46a5f8e9cd14aa1806b6df945dda55499db2fe71
cjBroom <90470010+cjBroom@users.noreply.github.com>
2023-04-18 11:44:56 -0400

    Created styles.module.css for top5AudioBooks and edited layout.tsx accordingly.
    Created styles.module.css for top5AudioBooks and edited layout.tsx accordingly.

--- app/catalog_home/top5AudioBooks/layout.tsx
+++ app/catalog_home/top5AudioBooks/layout.tsx
@@ -1,11 +1,15 @@
+import styles from './styles.module.css'
+
 export default function top5AudioBooksLayout({
     children, }: {
         children: React.ReactNode
     }) {
-    return (
+    
+        /* This css changes only the top5AudioBooks dir, and can be changed to make each fetch call unique to the given media. */
+        return (
         <>
             <nav>Please enjoy browsing the top 50 Audio Books from the iTunes store!</nav>
-            <main>
+            <main className={styles.main}>
                 {children}
             </main>
         </>
