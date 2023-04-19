cd0f436205f6c2e14117b24d25803cbb767606a8
Zoie Rast <zoie.m.rast@gmail.com>
2023-04-18 22:15:04 -0400

    Update page.tsx

--- app/profile/account/page.tsx
+++ app/profile/account/page.tsx
@@ -8,10 +8,14 @@ function Profile() {
         <div>
             <p><text className={"text-1xl font-bold"}>Edit Account Information</text></p>
             <form action="/send-data-here" method="post">
-                <p><label className="underline" htmlFor="first">First name:</label></p>
+                <p><label className="underline" htmlFor="pfp">Profile Picture:</label></p>
+                <input type="file" id="pfp" name="pfp" accept="image/png, image/jpeg"/>
+                <p><label className="underline" htmlFor="first">First Name:</label></p>
                 <input type="text" id="first" name="first"/>
-                <p><label className="underline" htmlFor="last">Last name:</label></p>
+                <p><label className="underline" htmlFor="last">Last Name:</label></p>
                 <input type="text" id="last" name="last"/>
+                <p><label className="underline" htmlFor="birthday">Birthday:</label></p>
+                <input type="date" id="birthday" name="birthday"/>
                 <p><label className="underline" htmlFor="address1">Address Line 1:</label></p>
                 <input type="text" id="address1" name="address1"/>
                 <p><label className="underline" htmlFor="address2">Address Line 2:</label></p>
@@ -21,9 +25,9 @@ function Profile() {
                 <p><label className="underline" htmlFor="state">State:</label></p>
                 <input type="text" id="state" name="state"/>
                 <p><label className="underline" htmlFor="email">Email:</label></p>
-                <input type="text" id="email" name="email"/>
+                <input type="email" id="email" name="email"/>
                 <p><label className="underline" htmlFor="phone">Phone Number:</label></p>
-                <input type="text" id="phone" name="phone"/>
+                <input type="number" id="phone" name="phone"/>
                 <p><button className="bg-white hover:verdigris text-payne-gray font-semibold hover:text-verdigris py-2 px-4 border border-black-olive hover:border-transparent rounded" type="submit">
                         Submit Changes
                 </button></p>
