8f1e030a4d053ff3ef949346f40d3b3ecd6e67a6
cjBroom <90470010+cjBroom@users.noreply.github.com>
2023-04-19 09:32:27 -0400

    Created 'head.tsx' to store Metadata for Profile Home
    Created 'head.tsx' to store Metadata for Profile Home

---
+++ app/profile/head.tsx
@@ -0,0 +1,16 @@
+/* 
+* The 'head' file containing Metadata is deprecated after version 13.2.
+* We are curently using version 13.1.6. 
+* We need to decide whether to upgrade to 13.2 or stay with 13.1.6 techniques. 
+ */
+
+export default function Head() {
+    return (
+        <>
+            <title>Profile Home</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="description" content="Created by The Brogrammers" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
